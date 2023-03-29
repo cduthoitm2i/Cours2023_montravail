1c69a59c67cd7329a976e02d3ef10a2329b79280
Christophe_M2i <cduthoit@live.fr>
2023-03-29 17:48:33 +0200

    ok

---
+++ 3-ALGO/detection min-maxi.php
@@ -0,0 +1,33 @@
+<?php
+define("MAX",10);
+
+echo "SAISIR UNE VALEUR {10}\n\r";
+$valeur;
+$cpt;
+$minValeur;
+$maxValeur;
+
+while($cpt<=MAX){
+	echo "Entre votre valeur ".$cpt." :\n\r";
+	$valeur=10;
+	$valeur=1000;
+	$valeur=5;
+	$valeur=150;
+	$valeur=8;
+	$valeur=88;
+	$valeur=75;
+	$valeur=250;
+	$valeur=14;
+	$valeur=6;
+	
+	if($cpt==1 || $val<$minValeur){
+		$minValeur = $valeur;
+	} elseif($cpt=1 || $val>$maxValeur){
+		$maxValeur = $valeur;
+	};
+	
+}
+
+echo "La valeur minimale est : ".$minValeur." :\n\r";
+echo "La valeur maximale est : ".$maxValeur." :\n\r";
+?>
